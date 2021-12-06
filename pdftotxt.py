20a15a725df9f50907a06deb3df446bac7962c77
Cockie085 <80370879+Cockie085@users.noreply.github.com>
2021-12-06 02:44:06 +0200

    Add files via upload

---
+++ pdftotxt.py
@@ -0,0 +1,25 @@
+import pdfminer
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+output_string = StringIO()
+#путь к файлу
+with open('your.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+a = output_string.getvalue().split('\n\n')
+#путь к файлу
+x = open('your.txt','a')
+for i in a:
+	x.write(i+'\n')
+	
